a4f407aaaec0ae34370bb7ddcdcc24ffff70fc6d
Magnus Granno <magnus@northtech.dk>
2026-04-03 17:57:15 +0200

    remove stats

--- src/lib/todo/todo-list.svelte
+++ src/lib/todo/todo-list.svelte
@@ -1,18 +1,15 @@
 <script lang="ts">
+	import { flip } from 'svelte/animate';
 	import { getTodoStore } from './todo-store.svelte';
 	import Todo from './todo.svelte';
 
 	const todoStore = getTodoStore();
-	const completedTodos = $derived(todoStore.todos.filter((todo) => todo.completed).length);
-	const totalTodos = $derived(todoStore.todos.length);
 </script>
 
 <section class="flex flex-col gap-1">
 	{#each todoStore.todos as todo (todo.id)}
-		<Todo {todo} />
+		<div animate:flip>
+			<Todo {todo} />
+		</div>
 	{/each}
-	<div class="flex items-center justify-between px-4 py-2">
-		<p class="text-xs text-neutral-500">{completedTodos} Completed</p>
-		<p class="text-xs text-neutral-500">{totalTodos} Total</p>
-	</div>
 </section>
